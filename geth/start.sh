9f3ec1865aa69b93b1715f43222f06ea1376b694
JefferyWa <sevafara@gmail.com>
2022-07-26 13:51:11 +0400

    Main Updated: start.sh

--- geth/start.sh
+++ geth/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 DATA_DIR=${DATA_DIR:-"/data"}
+GAS_LIMIT=${GAS_LIMIT:-"8000000"}
+GAS_PRICE=${GAS_PRICE:-"1000000000"}
 NETWORK_ID=${NETWORK_ID:-"1337"}
 UNLOCK_LIST=${UNLOCK_LIST:-""}
 
@@ -13,6 +15,9 @@ echo "Starting GETH..."
 geth --nousb \
   --maxpeers 0 \
   --datadir /data \
+  --mine \
+  --miner.gaslimit "$GAS_LIMIT" \
+  --miner.gasprice "$GAS_PRICE" \
   --networkid "$NETWORK_ID" \
   --http \
   --http.addr 0.0.0.0 \
